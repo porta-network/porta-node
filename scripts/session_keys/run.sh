f82c84720c752a2450bef0d86371bc04c018ec9e
shaneben23 <shanebenjamin.sb@gmail.com>
2021-08-08 13:20:24 +0000

    update run.sh

--- scripts/session_keys/run.sh
+++ scripts/session_keys/run.sh
@@ -1,7 +1,15 @@
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@babe1"
+curl http://46.101.22.15:9933  -H "Content-Type:application/json;charset=utf-8" -d "@babe1"
+curl http://46.101.55.87:9933  -H "Content-Type:application/json;charset=utf-8" -d "@babe2"
+curl http://46.101.73.55:9933  -H "Content-Type:application/json;charset=utf-8" -d "@babe3"
 
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@gran1"
+curl http://46.101.22.15:9933  -H "Content-Type:application/json;charset=utf-8" -d "@gran1"
+curl http://46.101.55.87:9933  -H "Content-Type:application/json;charset=utf-8" -d "@gran2"
+curl http://46.101.73.55:9933  -H "Content-Type:application/json;charset=utf-8" -d "@gran3"
 
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@imol1"
+curl http://46.101.22.15:9933  -H "Content-Type:application/json;charset=utf-8" -d "@imol1"
+curl http://46.101.55.87:9933  -H "Content-Type:application/json;charset=utf-8" -d "@imol2"
+curl http://46.101.73.55:9933  -H "Content-Type:application/json;charset=utf-8" -d "@imol3"
 
-curl http://localhost:9933  -H "Content-Type:application/json;charset=utf-8" -d "@audi1"
+curl http://46.101.22.15:9933  -H "Content-Type:application/json;charset=utf-8" -d "@audi1"
+curl http://46.101.55.87:9933  -H "Content-Type:application/json;charset=utf-8" -d "@audi2"
+curl http://46.101.73.55:9933  -H "Content-Type:application/json;charset=utf-8" -d "@audi3"
